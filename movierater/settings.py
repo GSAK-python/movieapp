1fd99c7c5819cf23436a1027e27a4b9606d0da10
Grzegorz Sakowski <you@example.com>
2020-01-21 10:36:38 +0100

    Wrzucenie moj.css do main/static oraz poprawienie local host

--- movierater/settings.py
+++ movierater/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://movierater-gsak.herokuapp.com', 'localhost', '.herokuapp.com', '.researchthroughdesign.org', 'http://127.0.0.1']
+ALLOWED_HOSTS = ['https://movierater-gsak.herokuapp.com', '127.0.0.1', 'localhost', '.herokuapp.com', '.researchthroughdesign.org']
 
 
 # Application definition
